6ab38b42c208de286efb8cd42a6d86be0f4d5e09
EugeneMazurok <audiofaun896373@gmail.com>
2024-04-05 20:52:15 +0300

    Initial Commit

---
+++ SebbiaProject/SebbiaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SebbiaProjectApp.swift
+//  SebbiaProject
+//
+//  Created by Евгений Мазурок on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SebbiaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
